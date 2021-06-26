ff74445685128e30b8d11a28957f3ded8a2e485d
TakekiMatsui <gtd1107@gmail.com>
2021-06-26 23:30:13 +0900

    main.js

---
+++ js-easy/js-basic/main.js
@@ -0,0 +1,14 @@
+// Step1 console.logとコンソール
+// console.log('Hello, World!');
+// console.log('こんにちは、世界！');
+// console.log(1 + 2);
+// console.log(10 - 4 *8);
+
+// 値の種類と変数
+console.log(100) // 数値
+console.log('hello') // 文字列
+console.log([1, 2, 3, 4, 5]) // 配列
+console.log(true) // 真偽値
+console.log(null) // 無
+console.log({ one: 1, two: 2 }) // オブジェクト
+
